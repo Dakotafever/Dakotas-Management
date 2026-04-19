0269728f8a2eca2c56f952fbce034f6d1f2414d4
Dakotafever <kirbydrogo@gmail.com>
2026-04-19 12:52:51 -0500

    Update suggestion format in suggest.js

--- api/suggest.js
+++ api/suggest.js
@@ -27,7 +27,7 @@ export default async function handler(req, res) {
         "Content-Type": "application/json"
       },
       body: JSON.stringify({
-        content: `💡 **New Suggestion:**\n${suggestion}`
+        content: `# New Suggestion / Fix / Idea!\n${suggestion}`
       })
     });
 
